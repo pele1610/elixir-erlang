4dc59f20818041e4e1d9602b4b69357ca7f8be17
pele1610 <pelecolo56@gmail.com>
2026-09-16 06:18:27 +1000

    refactor Calculator module to include arithmetic functions

--- functions.exs
+++ functions.exs
@@ -1,7 +1,17 @@
-defmodule Greeter do
-  def greet(name) do
-    IO.puts("Hello, #{name}!")
+defmodule Calculator do
+  def add(a, b) do
+    a + b
+  end
+
+  def subtract(a, b) do
+    a - b
+  end
+
+  def multiply(a, b) do
+    a * b
   end
 end
 
-Greeter.greet("Pele")
+IO.puts(Calculator.add(10, 5))
+IO.puts(Calculator.subtract(10, 5))
+IO.puts(Calculator.multiply(10, 5))
